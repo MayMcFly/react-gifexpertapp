c3abba3b21ff63d82e303ab32b471d49c7ced63e
maygallegos <mg.onze@gmail.com>
2020-07-24 13:57:14 -0500

    docs agregado

---
+++ docs/precache-manifest.643139dc165f41498721ac96ecad7d0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef2e37a6912570e7c93eff8126227209",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8659fce4483d148525bf",
+    "url": "/static/css/main.610c18d0.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8659fce4483d148525bf",
+    "url": "/static/js/main.5fe3cf8d.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
